4f0dc3133a48051d5506f659e7779aca17b4c799
Elektropay <richard.r@everpayinc.com>
2024-06-05 03:51:58 -0500

    Create index.js

---
+++ src/components/AmlPolicyHeroSection/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  HeroBg,
+  HeroBtnWrapper,
+  HeroContainer,
+  HeroContent,
+  HeroH1,
+  HeroP,
+} from "./AmlpolicyElements";
+
+export const AmlPolicy = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Anti-Money Laundering Policy</HeroH1>
+        <HeroP>
+        We implemented best-of-class measures to combat financial crimes.
+        </HeroP>
+        <HeroBtnWrapper>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
