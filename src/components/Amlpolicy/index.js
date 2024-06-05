2bae9216793fca47ccd2f5ad81358fcf4ce2069e
Elektropay <richard.r@everpayinc.com>
2024-06-05 03:05:35 -0500

    Update index.js

--- src/components/Amlpolicy/index.js
+++ src/components/Amlpolicy/index.js
@@ -29,21 +29,21 @@ export const AmlPolicy = () => {
       </HeroContent>
     </HeroContainer>
         <Content>
-          <H1>AML Policy</H1>
             <Text>
-                            <H4>We're always looking for new ways to provide privacy for our customers.</4>
-                            <p>Id ipsum mi tempor eget. Pretium consectetur scelerisque blandit habitasse non ullamcorper enim, diam quam id et, tempus massa. Sed nam vulputate pellentesque quis. Varius a, nunc faucibus proin elementum id odio auctor.
-                                Nunc, suspendisse consequat libero, pharetra tellus vulputate auctor venenatis tortor non rhoncus at duis. Pharetra ipsum mauris integer sit feugiat.</p>
+                        <H1>AML Policy</H1>
+                            <H4>We're always looking for new ways to provide privacy for our customers.</H4>
+                            <P>Id ipsum mi tempor eget. Pretium consectetur scelerisque blandit habitasse non ullamcorper enim, diam quam id et, tempus massa. Sed nam vulputate pellentesque quis. Varius a, nunc faucibus proin elementum id odio auctor.
+                                Nunc, suspendisse consequat libero, pharetra tellus vulputate auctor venenatis tortor non rhoncus at duis. Pharetra ipsum mauris integer sit feugiat.</P>
                             <ul>
                                 <li>— Blandit dignissim nulla varius tristique a sed integer ut tempor.</li>
                                 <li>— Augue interdum semper bibendum amet sed.</li>
                                 <li>— Dis in at ultricies tortor sit tellus.</li>
                                 <li>— Habitant ornare aenean maecenas pretium</li>
                             </ul>
-                            <p>Eget purus aenean sit risus. Arcu, aliquam eget et viverra risus purus. Commodo fames tristique dui pharetra elit aliquet morbi. Eget consectetur risus nunc lorem sit consequat aliquet. Dolor velit consectetur etiam scelerisque. Integer interdum sodales scelerisque diam massa quam sit quis. Sed et dui a nam pulvinar. Tristique justo, donec lectus vitae, cursus ligula ridiculus lacus, tincidunt. Diam dictumst faucibus dui aliquet aenean nascetur feugiat leo. Etiam dignissim orci dignissim vitae.</p>
+                            <P>Eget purus aenean sit risus. Arcu, aliquam eget et viverra risus purus. Commodo fames tristique dui pharetra elit aliquet morbi. Eget consectetur risus nunc lorem sit consequat aliquet. Dolor velit consectetur etiam scelerisque. Integer interdum sodales scelerisque diam massa quam sit quis. Sed et dui a nam pulvinar. Tristique justo, donec lectus vitae, cursus ligula ridiculus lacus, tincidunt. Diam dictumst faucibus dui aliquet aenean nascetur feugiat leo. Etiam dignissim orci dignissim vitae.</p>
                             <Safedata>
                                 <H4>Your data is safe with us, we will not share any information with external sources.</H4>
-                                <p>Blandit dignissim nulla varius tristique a sed integer ut tempor. Augue interdum semper bibendum amet sed. Dis in at ultricies tortor sit tellus. Habitant ornare aenean maecenas pretium, dui ullamcorper quis. Egestas viverra et id aliquet faucibus rhoncus a. Sollicitudin nisl nulla tempor pretium lorem at mauris faucibus pulvinar.</p>
+                                <P>Blandit dignissim nulla varius tristique a sed integer ut tempor. Augue interdum semper bibendum amet sed. Dis in at ultricies tortor sit tellus. Habitant ornare aenean maecenas pretium, dui ullamcorper quis. Egestas viverra et id aliquet faucibus rhoncus a. Sollicitudin nisl nulla tempor pretium lorem at mauris faucibus pulvinar.</P>
                                 <ul>
                                     <li>Eget purus aenean sit risus. Arcu, aliquam eget et viverra risus purus. Commodo fames tristique dui pharetra elit aliquet morbi.</li>
                                     <li>Eget consectetur risus nunc lorem sit consequat aliquet. Dolor velit consectetur etiam scelerisque.</li>
@@ -54,7 +54,7 @@ export const AmlPolicy = () => {
                             </Safedata>
                             <Cookies>
                                 <H4>Cookies and tracking</H4>
-                                <p>Consequat mauris nunc congue nisi. Cursus metus aliquam eleifend mi in nulla. Dignissim cras tincidunt lobortis feugiat vivamus. Blandit aliquam etiam erat velit. Sapien faucibus et molestie ac. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin.</p>
+                                <P>Consequat mauris nunc congue nisi. Cursus metus aliquam eleifend mi in nulla. Dignissim cras tincidunt lobortis feugiat vivamus. Blandit aliquam etiam erat velit. Sapien faucibus et molestie ac. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin.</P>
                             </Cookies>
             </Text>
         </Content>
