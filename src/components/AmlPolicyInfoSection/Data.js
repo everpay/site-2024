92b1a44b43908419c875ddfb4b727945c331fd59
Elektropay <richard.r@everpayinc.com>
2024-06-05 04:28:11 -0500

    Create Data.js

---
+++ src/components/AmlPolicyInfoSection/Data.js
@@ -0,0 +1,53 @@
+export const AmlPolicyObjOne = {
+  id: "merchanthub",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Now with global payouts",
+  headline: "Merchant Hub",
+  description:
+    "Merchant Hub is a feature complete, payments and payout platform that enables you to customize various account-centric product experiences including - Credit Card Acquiring, Alternative Payment Methods, P2P payments, Crypto payments and payouts.",
+  buttonLabel: "Explore MerchantHub",
+  imgStart: true,
+  img: require("../../images/svg-2.svg").default,
+  alt: "Payments",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const AmlPolicyObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Multi-Currency Accounts",
+  headline: "Payment Solutions",
+  description:
+    "Let your customers pay wherever and however they prefer. Work with one provider to accept, process, and settle payments 24/7.",
+  buttonLabel: "Learn More",
+  imgStart: false,
+  img: require("../../images/svg-5.svg").default,
+  alt: "multi-currency accounts",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const AmlPolicyObjThree = {
+  id: "developers",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "The developers platform paradise",
+  headline: "Accept Payments Everywhere",
+  description:
+    "Now teams can build and cut development times through one platform with complete unified payments and banking as a service functionality. Get instant access to over 1000's of payment gateways, fintech providers, payments and crypto rails that make up our global infrastructure.",
+  buttonLabel: "Read the docs",
+  imgStart: true,
+  img: require("../../images/svg-6.svg").default,
+  alt: "Paper",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
